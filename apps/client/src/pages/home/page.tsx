8f58b04542bee960390fce7be77e83ba4cf72c4b
mohankumarpaluru <mohankumarpaluru@gmail.com>
2025-02-15 21:55:04 +0530

    updated OG

--- apps/client/src/pages/home/page.tsx
+++ apps/client/src/pages/home/page.tsx
@@ -21,12 +21,12 @@ export const HomePage = () => {
         <html lang={i18n.locale} />
 
         <title>
-          {t`Reactive Resume`} - {t`A free and open-source resume builder`}
+          Trix CV - A powerful, privacy-focused resume builder
         </title>
 
         <meta
           name="description"
-          content="A free and open-source resume builder that simplifies the process of creating, updating, and sharing your resume."
+          content="Create, customize, and share your resume effortlessly with an open-source, ad-free platform."
         />
       </Helmet>
 
